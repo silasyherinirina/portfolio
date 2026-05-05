1702ff155a48696c1042cc733c63588fecad7c6a
silasyherinirina <livajaonasilasy@gmail.com>
2026-05-05 15:53:57 +0200

    update portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,8 @@ export default function Contact() {
     return (
         <section className="p-10 bg-gray-900 text-white text-center">
             <h2 className="text-3xl font-bold">Contact</h2>
-            <p className="mt-4">Email : livajaonasilasy@gmail.com</p>
+            <p className="mt-4">Contact: 033 14 156 62</p>
+            <p>Email : livajaonasilasy@gmail.com</p>
             <p>GitHub : github.com/silasyherinirina</p>
         </section>
     );

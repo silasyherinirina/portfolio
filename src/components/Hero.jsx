7899a22d3e8696f20fd9383a0fc44c0f786c2490
silasyherinirina <livajaonasilasy@gmail.com>
2026-05-05 16:18:53 +0200

    update portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
+import photo from "../assets/my photo.jpg";
 export default function Hero() {
     return (
         <section className="min-h-screen flex flex-col justify-center items-center text-center bg-gradient-to-r from-gray-900 to-black text-white">
+
+            {/* 📸 PHOTO */}
+            <img
+                src={photo}
+                alt="Liva Silasy"
+                className="w-40 h-40 rounded-full object-cover border-4 border-white shadow-lg mb-6"
+            />
+
+            {/* 🧑 NOM */}
             <h1 className="text-5xl md:text-6xl font-bold">
                 Liva Jaona Silasy
             </h1>
